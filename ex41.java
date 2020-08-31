696eee140f671af862583d91ca27e1597d3b7604
7248510 <calebkre19@gmail.com>
2020-08-31 13:47:50 -0400

    Pseudo stage

---
+++ ex41.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.lang.*;
+//Java classes can't start with a number
+class Listing
+{
+    String name; //Declaring
+    int age; //Declaring
+    name = " "; //Setting values
+    age = 0; //Setting values
+    void input()
+    {      
+
+    }
+    void setName(name)
+    {
+
+    }
+
+    void setAge(String)
+    {
+
+    }
+
+    String getName()
+    {
+
+    }
+
+    int getAge()
+    {
+
+    }   
+}
+
+public class ex41
+{
+    public static void main(String args[]) //Not passing any arguments on the command line.
+    {
+        System.out.println("Hello friend");
+    }
+}
